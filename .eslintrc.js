6a036f6e7fc6cbabe10333a3a06f8736caf7f934
Joshua Sosso <josh@modiimedia.com>
2021-03-11 22:39:16 -0600

    add starter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,25 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'html'],
-  rules: {},
+  rules: {
+    'prefer-destructuring': 0,
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+    ],
+  },
 };
