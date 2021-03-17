6b75e9f3bfab94ff1bde1a10446b2fc6e93a4c48
Joshua Sosso <josh@modiimedia.com>
2021-03-17 01:17:31 -0500

    fix purgecss settings

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
 const { join } = require('path');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./hugo_stats.json'],
+  defaultExtractor: (content) => {
+    const els = JSON.parse(content).htmlElements;
+    return els.tags.concat(els.classes, els.ids);
+  },
+});
 
 module.exports = {
-    plugins: [
-        require('postcss-import')({
-            path: [__dirname],
-        }),
-        require('tailwindcss')(join(__dirname, 'tailwind.config.js')),
-        require('autoprefixer')({
-            grid: true,
-            browsers: ['>1%'],
-        }),
-    ],
+  plugins: [
+    require('postcss-import')({
+      path: [__dirname],
+    }),
+    require('tailwindcss')(join(__dirname, 'tailwind.config.js')),
+    require('autoprefixer')({
+      grid: true,
+      browsers: ['>1%'],
+    }),
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : []),
+  ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,4 @@ module.exports = {
     theme: {},
     darkMode: false,
     plugins: [],
-    purge: {
-        enabled: process.env.NODE_ENV === 'production',
-        content: [
-            './layouts/**/*.html',
-            './assets/**/*.ts',
-            './assets/**/*.scss',
-            './assets/**/*.css',
-            './typescript/**/*.ts',
-        ],
-    },
 };
