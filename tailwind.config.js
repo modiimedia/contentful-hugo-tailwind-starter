b7543acd0f236e3ba11247fb1e671900a693ae77
Josh Michaels <31548851+joshmossas@users.noreply.github.com>
2022-02-26 21:46:40 -0600

    update tailwind to 3 and other libs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    theme: {},
-    darkMode: false,
+    content: [
+        './layouts/**/*.html',
+        './typescript/**/*.ts',
+        './typescript/**/*.js',
+        './typescript/**/*.vue',
+        './typescript/**/*.tsx',
+        './typescript/**/*.jsx',
+    ],
     plugins: [],
 };
